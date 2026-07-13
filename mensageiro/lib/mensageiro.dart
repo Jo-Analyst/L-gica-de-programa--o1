bbb8d3f8b4a9b1b465c4fd74caa3d6cdde7bc0e1
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2026-07-13 10:19:18 -0300

    mensageiro 3

--- mensageiro/lib/mensageiro.dart
+++ mensageiro/lib/mensageiro.dart
@@ -14,12 +14,46 @@ void main() {
       case '4':
         continuar = false;
       case '1':
-        print('Busca mensagem');
+        buscarTodasAsMensagens(mensageiro);
+      case '2':
+        enviarMensagem(mensageiro);
+      case '3':
+        print('Remover mensangem');
+      default:
+        print('Opção inválida');
+    }
+
+    if (continuar) {
+      stdout.write('\nPressione a tecla ENTER para continuar...');
+      stdin.readLineSync();
     }
   }
 }
 
+void enviarMensagem(MensageiroRepository mensageiro) {
+  stdout.write('Digite a sua mensagem: ');
+  String texto = stdin.readLineSync()!;
+  stdout.write('Digite o contato: ');
+  String contato = stdin.readLineSync()!;
+  mensageiro.enviarMensagem(texto: texto, contato: contato);
+  print('Mensagem enviado com sucesso!');
+}
+
+void buscarTodasAsMensagens(MensageiroRepository mensageiro) {
+  var mensagens = mensageiro.buscarTodasAsMensagens();
+
+  if (mensagens.isEmpty) return;
+
+  mostrarCabecalho(mensagem: 'LISTA DE MENSAGENS');
+  for (var mensagem in mensagens) {
+    print(
+      'ID: ${mensagem.id} | Nome: ${mensagem.texto} | Contato: ${mensagem.contato}',
+    );
+  }
+}
+
 void mostrarMenu() {
+  mostrarCabecalho(mensagem: 'MENU PRINCIPAL');
   print('1 - Mostrar Mensagem');
   print('2 - Enviar Mensagem');
   print('3 - remover mensagem');
